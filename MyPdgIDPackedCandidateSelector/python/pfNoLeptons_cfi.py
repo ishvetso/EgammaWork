d4d58529dc41cf09c3be561a5f533750f8b109be
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-04-23 11:33:26 +0200

    delete rubbish

--- MyPdgIDPackedCandidateSelector/python/pfNoLeptons_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfNoLeptons = cms.EDProducer("MyPdgIDPackedCandidateSelector",
-    src = cms.InputTag("packedPFCandidates"),
-    pdgId = cms.vint32(11,-11, 13,-13)
-)
-
