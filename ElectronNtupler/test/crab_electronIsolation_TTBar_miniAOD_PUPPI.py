e7c36fe00ee1576c9b9edcf48ccae7d63501e551
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-04-28 16:54:33 +0200

    crab configs for miniAOD

---
+++ ElectronNtupler/test/crab_electronIsolation_TTBar_miniAOD_PUPPI.py
@@ -0,0 +1,19 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_("General")
+config.General.requestName = 'Electron-Isolation_CITK_validation_ttbar_miniAOD_PUPPI_with_NoLeptons_update23April2015'
+config.General.workArea = 'crab_projects'
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/afs/cern.ch/work/i/ishvetso/EgammaWork/ElectronIsolationPUPPI_updated21April2014/CMSSW_7_3_3/src/EgammaWork/electron_isolation_CITK_PUPPI.py'
+config.section_("Data")
+config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1 
+#config.Data.outLFN = '/store/user/<subdir>' # or '/store/group/<subdir>'
+config.Data.publication = True
+#config.Data.Data.publishDBS = 'phys03'
+config.Data.publishDataName = 'Electron-Isolation_CITK_validation_ttbar_miniAOD_PUPPI_with_NoLeptons_updated23April2015'
+config.section_("Site")
+config.Site.storageSite = 'T2_DE_DESY'
