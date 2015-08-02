800e49d09148ffff4620762749232de4ffd9848a
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-08-02 16:27:33 +0200

    add crab configs

---
+++ MuonNtupler/test/crab_muonIsolation_DY_miniAOD_PUPPI.py
@@ -0,0 +1,19 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_("General")
+config.General.requestName = 'MuonIsolation_First'
+config.General.workArea = 'crab_projects'
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/afs/cern.ch/work/i/ishvetso/EgammaWork/MuonIsolation/CMSSW_7_4_6_patch2/src/EgammaWork/muon_isolatin_CITK.py'
+config.section_("Data")
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+#config.Data.outLFN = '/store/user/<subdir>' # or '/store/group/<subdir>'
+config.Data.publication = False
+#config.Data.Data.publishDBS = 'phys03'
+config.Data.publishDataName = 'MuonIsolation_First'
+config.section_("Site")
+config.Site.storageSite = 'T2_DE_DESY'
