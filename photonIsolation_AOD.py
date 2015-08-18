038b8a0c193e2e00d1488e1c5ad9bbdf4c0dc4c9
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-08-18 17:20:16 +0200

    add config for AOD case

---
+++ photonIsolation_AOD.py
@@ -0,0 +1,170 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process( "PhotonIsoTest" )
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'MCRUN2_74_V9::All'
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+process.options.allowUnscheduled = cms.untracked.bool(False) 
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:///afs/cern.ch/work/i/ishvetso/EgammaWork/test_samples/ttbar_AOD.root')
+)
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+
+process.load("CommonTools.ParticleFlow.pfNoPileUpIso_cff")
+process.load("CommonTools.ParticleFlow.pfParticleSelection_cff")
+
+process.pfNoPileUpCandidates = process.pfAllChargedHadrons.clone()
+process.pfNoPileUpCandidates.pdgId.extend(process.pfAllNeutralHadronsAndPhotons.pdgId)
+
+process.particleFlowTmpPtrs = cms.EDProducer("PFCandidateFwdPtrProducer",
+src = cms.InputTag('particleFlow')
+)
+
+process.egmPhotonIsolationAOD = cms.EDProducer( "CITKPFIsolationSumProducer",
+			  srcToIsolate = cms.InputTag("gedPhotons"),
+			  srcForIsolationCone = cms.InputTag('pfNoPileUpCandidates'),
+			  isolationConeDefinitions = cms.VPSet(
+			   cms.PSet( isolationAlgo = cms.string('PhotonPFIsolationWithMapBasedVeto'), 
+				      coneSize = cms.double(0.3),
+				      isolateAgainst = cms.string('h+'),
+				      miniAODVertexCodes = cms.vuint32(3),
+				      vertexIndex = cms.int32(0),
+				    ),
+			   cms.PSet( isolationAlgo = cms.string('PhotonPFIsolationWithMapBasedVeto'), 
+				      coneSize = cms.double(0.3),
+				      isolateAgainst = cms.string('h0'),
+				      miniAODVertexCodes = cms.vuint32(2,3),
+				      vertexIndex = cms.int32(0),
+				    ),
+			   cms.PSet( isolationAlgo = cms.string('PhotonPFIsolationWithMapBasedVeto'), 
+				      coneSize = cms.double(0.3),
+				      isolateAgainst = cms.string('gamma'),
+				      miniAODVertexCodes = cms.vuint32(2,3),
+				      vertexIndex = cms.int32(0),
+				    )
+    )
+  )	
+
+process.egmPhotonIsolationAODDcuts = cms.EDProducer( "CITKPFIsolationSumProducer",
+        srcToIsolate = cms.InputTag("gedPhotons"),
+        srcForIsolationCone = cms.InputTag('pfNoPileUpCandidates'),
+        isolationConeDefinitions = cms.VPSet(
+         cms.PSet( isolationAlgo = cms.string('PhotonPFIsolationWithMapBasedVeto_dzcut'), 
+              coneSize = cms.double(0.3),
+              isolateAgainst = cms.string('h+'),
+              miniAODVertexCodes = cms.vuint32(3),
+              vertexIndex = cms.int32(0),
+              vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+            ),
+         cms.PSet( isolationAlgo = cms.string('PhotonPFIsolationWithMapBasedVeto_dzcut'), 
+              coneSize = cms.double(0.3),
+              isolateAgainst = cms.string('h0'),
+              miniAODVertexCodes = cms.vuint32(2,3),
+              vertexIndex = cms.int32(0),
+              vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+            ),
+         cms.PSet( isolationAlgo = cms.string('PhotonPFIsolationWithMapBasedVeto_dzcut'), 
+              coneSize = cms.double(0.3),
+              isolateAgainst = cms.string('gamma'),
+              miniAODVertexCodes = cms.vuint32(2,3),
+              vertexIndex = cms.int32(0),
+              vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+            )
+    )
+  ) 
+			   
+process.photonIDValueMapProducer = cms.EDProducer('PhotonIDValueMapProducer',
+                                          # The module automatically detects AOD vs miniAOD, so we configure both
+                                          #
+                                          # AOD case
+                                          #
+                                          ebReducedRecHitCollection = cms.InputTag("reducedEcalRecHitsEB"),
+                                          eeReducedRecHitCollection = cms.InputTag("reducedEcalRecHitsEE"),
+                                          esReducedRecHitCollection = cms.InputTag("reducedEcalRecHitsES"),
+                                          particleBasedIsolation = cms.InputTag("particleBasedIsolation","gedPhotons"),
+                                          vertices = cms.InputTag("offlinePrimaryVertices"),
+                                          pfCandidates = cms.InputTag("particleFlow"),
+                                          src = cms.InputTag('gedPhotons'),
+                                          #
+                                          # miniAOD case
+                                          #
+                                          ebReducedRecHitCollectionMiniAOD = cms.InputTag("reducedEgamma:reducedEBRecHits"),
+                                          eeReducedRecHitCollectionMiniAOD = cms.InputTag("reducedEgamma:reducedEERecHits"),
+                                          esReducedRecHitCollectionMiniAOD = cms.InputTag("reducedEgamma:reducedESRecHits"),
+                                          verticesMiniAOD = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                                          pfCandidatesMiniAOD = cms.InputTag("packedPFCandidates"),
+                                          # there is no need for the isolation map here, for miniAOD it is inside packedPFCandidates
+                                          srcMiniAOD = cms.InputTag('slimmedPhotons'),
+                                          )
+
+process.ntupler = cms.EDAnalyzer('SimplePhotonNtupler',
+                                 # The module automatically detects AOD vs miniAOD, so we configure both
+                                 #
+                                 # Common to all formats objects
+                                 #                                    
+                                 rho = cms.InputTag("fixedGridRhoFastjetAll"),
+                                 #
+                                 # Objects specific to AOD format
+                                 #
+                                 photons = cms.InputTag("gedPhotons"),
+                                 genParticles = cms.InputTag("genParticles"),
+                                 #
+                                 # Objects specific to MiniAOD format
+                                 #
+                                 photonsMiniAOD = cms.InputTag("slimmedPhotons"),
+                                 genParticlesMiniAOD = cms.InputTag("prunedGenParticles"),
+                                 #
+                                 # ValueMap names from the producer upstream
+                                 #
+                                 full5x5SigmaIEtaIEtaMap   = cms.InputTag("photonIDValueMapProducer:phoFull5x5SigmaIEtaIEta"),
+                                 phoChargedIsolation = cms.InputTag("photonIDValueMapProducer:phoChargedIsolation"),
+                                 phoNeutralHadronIsolation = cms.InputTag("photonIDValueMapProducer:phoNeutralHadronIsolation"),
+                                 phoPhotonIsolation = cms.InputTag("photonIDValueMapProducer:phoPhotonIsolation"),
+                                 
+                                 #CITK
+                                 phoChargedIsolation_CITK = cms.InputTag("egmPhotonIsolationAOD:h+-DR030-"),
+                                 phoNeutralHadronIsolation_CITK = cms.InputTag("egmPhotonIsolationAOD:h0-DR030-"),
+                                 phoPhotonIsolation_CITK = cms.InputTag("egmPhotonIsolationAOD:gamma-DR030-"),
+                                 #CITK dcuts
+                                 phoChargedIsolation_CITK_dcuts = cms.InputTag("egmPhotonIsolationAODDcuts:h+-DR030-"),
+                                 phoNeutralHadronIsolation_CITK_dcuts = cms.InputTag("egmPhotonIsolationAODDcuts:h0-DR030-"),
+                                 phoPhotonIsolation_CITK_dcuts = cms.InputTag("egmPhotonIsolationAODDcuts:gamma-DR030-"),
+                                 # 
+                                 # Locations of files with the effective area constants.
+                                 # The constants in these files below are derived for PHYS14 MC.
+                                 #
+                                 effAreaChHadFile = cms.FileInPath
+                                 ("RecoEgamma/PhotonIdentification/data/PHYS14/effAreaPhotons_cone03_pfChargedHadrons_V2.txt"),
+                                 effAreaNeuHadFile= cms.FileInPath
+                                 ("RecoEgamma/PhotonIdentification/data/PHYS14/effAreaPhotons_cone03_pfNeutralHadrons_V2.txt"),
+                                 effAreaPhoFile   = cms.FileInPath
+                                 ("RecoEgamma/PhotonIdentification/data/PHYS14/effAreaPhotons_cone03_pfPhotons_V2.txt")
+                                )			   
+
+process.analysis = cms.Path( process.particleFlowTmpPtrs  + process.pfParticleSelectionSequence + process.pfNoPileUpCandidates + process.egmPhotonIsolationAOD + process.egmPhotonIsolationAODDcuts + process.photonIDValueMapProducer + process.ntupler)
+
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+
+'''
+process.out = cms.OutputModule("PoolOutputModule",
+                               fileName = cms.untracked.string('patTuple_miniAOD.root'),
+                               outputCommands = cms.untracked.vstring('keep *')
+                               )                            
+
+                           
+process.outpath = cms.EndPath(process.out)
+'''
+process.TFileService = cms.Service("TFileService",
+                                 fileName = cms.string("tree.root")
+                                  )
