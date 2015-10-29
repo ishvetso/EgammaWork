12383c3ac7c1c7f1862a4a4ac105dda556ac9390
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2015-10-29 10:36:30 +0100

    minor

--- PhotonNtupler/test/ROC_drawer.cpp
+++ PhotonNtupler/test/ROC_drawer.cpp
@@ -26,7 +26,7 @@
    tree -> Project("bkg_abs", var.c_str(), (BkgSelection + "&&" + addSelection) .c_str());
    
    
-  for (int iBin = 2; iBin <= Nbins ; iBin += 1)
+  for (int iBin = 1; iBin <= Nbins ; iBin += 1)
   {
     float effSig = (float) (hist_sig -> Integral(1, iBin))/(hist_sig_abs -> Integral());
     float effBkg = (float) (hist_bkg -> Integral(1, iBin))/(hist_bkg_abs -> Integral());
@@ -50,10 +50,10 @@ void ROC_Drawer::draw_ROC()
   gStyle->SetOptTitle(0);
 //gPad->SetGrid();
   
-  TGraph * gr1 =  graph_ROC("relisoWithEA", 10000, 0., 10.);
-  TGraph * gr2 =  graph_ROC("relisoWithEA_CITK_", 10000, 0., 10.);
-  TGraph * gr3 =  graph_ROC("relisoWithEA_PUPPI", 10000, 0., 10.);
-  TGraph * gr4 =  graph_ROC("relisoWithEA_pf", 10000, 0., 10.);
+
+  TGraph * gr1 =  graph_ROC("relisoWithEA_CITK_", 10000, 0., 10.);
+  TGraph * gr2 =  graph_ROC("relisoWithEA_PUPPI", 10000, 0., 10.);
+  TGraph * gr3 =  graph_ROC("relisoWithEA_pf", 10000, 0., 10.);
   
   
   gr1 -> GetYaxis() -> SetTitle("bkg rejection");
@@ -62,12 +62,10 @@ void ROC_Drawer::draw_ROC()
   gr1 -> SetLineWidth(2.);
   gr2 -> SetLineWidth(2.);
   gr3 -> SetLineWidth(2.);
-  gr4 -> SetLineWidth(2.);
   
   gr1 -> SetLineColor(kBlue);
-  gr2 -> SetLineColor(kGreen+2);
+  gr2 -> SetLineColor(kGreen);
   gr3 -> SetLineColor(kRed);
-  gr4 -> SetLineColor(kYellow + 1);
   
   setTDRStyle();   
   TCanvas *c1= new TCanvas("c1","canvas",1200,800);
@@ -75,17 +73,17 @@ void ROC_Drawer::draw_ROC()
   TLegend *leg = new TLegend(0.2,0.3,0.5,0.7);
   leg -> SetFillColor(kWhite);  
   
-  leg->AddEntry(gr1, "relisoWithEA","l");
-  leg->AddEntry(gr2, "relisoWithEA_CITK_","l");
-  leg->AddEntry(gr3, "relisoWithEA_PUPPI","l");
-  leg->AddEntry(gr4, "relisoWithEA_pf","l");
+ 
+  leg->AddEntry(gr1, "relisoWithEA_CITK_","l");
+  leg->AddEntry(gr2, "relisoWithEA_PUPPI","l");
+  leg->AddEntry(gr3, "relisoWithEA_pf","l");
   
  
   c1 -> cd();
   gr1 -> Draw();
   gr2 -> Draw("SAME");
   gr3 -> Draw("SAME");
-  gr4 -> Draw("SAME");
+  
   
   leg -> Draw("SAME");
   CMS_lumi( c1, 4, 0 );
