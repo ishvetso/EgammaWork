ac44c8ad664483a9502aca82f5d755ae1796dc1b
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-04-10 18:23:11 +0200

    update

--- photonIsolation_AOD.py
+++ photonIsolation_AOD.py
@@ -17,19 +17,11 @@ process.source = cms.Source("PoolSource",
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load("CommonTools.PileupAlgos.Puppi_cff")
 
-process.load("CommonTools.ParticleFlow.pfNoPileUpIso_cff")
-process.load("CommonTools.ParticleFlow.pfParticleSelection_cff")
+from RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationPUPPI_cff import egmPhotonIsolationAODPUPPI
 
-process.pfNoPileUpCandidates = process.pfAllChargedHadrons.clone()
-process.pfNoPileUpCandidates.pdgId.extend(process.pfAllNeutralHadronsAndPhotons.pdgId)
-
-process.particleFlowTmpPtrs = cms.EDProducer("PFCandidateFwdPtrProducer",
-src = cms.InputTag('particleFlow')
-)
-from RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationAOD_cff import egmPhotonIsolationAOD
-
-process.egmPhotonIsolationAOD = egmPhotonIsolationAOD.clone()
+process.egmPhotonIsolationAODPUPPI = egmPhotonIsolationAODPUPPI.clone()
 
 process.ntupler = cms.EDAnalyzer('SimplePhotonNtupler',
                                  # The module automatically detects AOD vs miniAOD, so we configure both
@@ -48,9 +40,9 @@ process.ntupler = cms.EDAnalyzer('SimplePhotonNtupler',
                                  photonsMiniAOD = cms.InputTag("slimmedPhotons"),
                                  genParticlesMiniAOD = cms.InputTag("prunedGenParticles"),
                                  #Value maps from CITK
-                                 phoChargedIsolation_CITK = cms.InputTag("egmPhotonIsolationAOD:h+-DR030-"),
-                                 phoNeutralHadronIsolation_CITK = cms.InputTag("egmPhotonIsolationAOD:h0-DR030-"),
-                                 phoPhotonIsolation_CITK = cms.InputTag("egmPhotonIsolationAOD:gamma-DR030-"),
+                                 phoChargedIsolation_CITK = cms.InputTag("egmPhotonIsolationAODPUPPI:h+-DR030-"),
+                                 phoNeutralHadronIsolation_CITK = cms.InputTag("egmPhotonIsolationAODPUPPI:h0-DR030-"),
+                                 phoPhotonIsolation_CITK = cms.InputTag("egmPhotonIsolationAODPUPPI:gamma-DR030-"),
                                  # 
                                  # Locations of files with the effective area constants.
                                  # The constants in these files below are derived for PHYS14 MC.
@@ -64,7 +56,7 @@ process.ntupler = cms.EDAnalyzer('SimplePhotonNtupler',
                                  genInfo = cms.InputTag("generator")
                                 )			   
 
-process.analysis = cms.Path(process.particleFlowTmpPtrs +  process.pfParticleSelectionSequence + process.pfNoPileUpCandidates + process.egmPhotonIsolationAOD +  process.ntupler)
+process.analysis = cms.Path(process.puppi + process.egmPhotonIsolationAODPUPPI +  process.ntupler)
 
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
