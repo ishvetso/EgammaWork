83bb6acb76ce7bda5f825455b36fac13a801365e
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-04-10 18:38:45 +0200

    update

---
+++ egmPhotonIsolationPUPPI_miniAODrerunPUPPI.py
@@ -0,0 +1,71 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process( "PhotonIsolation" )
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = '76X_mcRun2_asymptotic_RunIIFall15DR76_v1'
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+process.options.allowUnscheduled = cms.untracked.bool(False) 
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('/store/mc/RunIIFall15MiniAODv1/GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/20000/167B87FB-84AC-E511-A144-0CC47A745298.root')
+)
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load("CommonTools.PileupAlgos.Puppi_cff")
+process.puppi.candName = cms.InputTag('packedPFCandidates')
+process.puppi.vertexName = cms.InputTag('offlineSlimmedPrimaryVertices')
+
+from RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationPUPPI_cff import egmPhotonIsolationMiniAODPUPPI
+
+process.egmPhotonIsolationMiniAODPUPPI = egmPhotonIsolationMiniAODPUPPI.clone()
+process.egmPhotonIsolationMiniAODPUPPI.puppiValueMap = cms.InputTag('puppi')
+
+
+process.ntupler = cms.EDAnalyzer('SimplePhotonNtupler',
+                                 # The module automatically detects AOD vs miniAOD, so we configure both
+                                 #
+                                 # Common to all formats objects
+                                 #                                    
+                                 rho = cms.InputTag("fixedGridRhoFastjetAll"),
+                                 #
+                                 # Objects specific to AOD format
+                                 #
+                                 photons = cms.InputTag("gedPhotons"),
+                                 genParticles = cms.InputTag("genParticles"),
+                                 #
+                                 # Objects specific to MiniAOD format
+                                 #
+                                 photonsMiniAOD = cms.InputTag("slimmedPhotons"),
+                                 genParticlesMiniAOD = cms.InputTag("prunedGenParticles"),
+                                 #Value maps from CITK
+                                 phoChargedIsolation_CITK = cms.InputTag("egmPhotonIsolationMiniAODPUPPI:h+-DR030-"),
+                                 phoNeutralHadronIsolation_CITK = cms.InputTag("egmPhotonIsolationMiniAODPUPPI:h0-DR030-"),
+                                 phoPhotonIsolation_CITK = cms.InputTag("egmPhotonIsolationMiniAODPUPPI:gamma-DR030-"),
+                                 # 
+                                 # Locations of files with the effective area constants.
+                                 # The constants in these files below are derived for PHYS14 MC.
+                                 #
+                                 effAreaChHadFile = cms.FileInPath
+                                 ("RecoEgamma/PhotonIdentification/data/PHYS14/effAreaPhotons_cone03_pfChargedHadrons_V2.txt"),
+                                 effAreaNeuHadFile= cms.FileInPath
+                                 ("RecoEgamma/PhotonIdentification/data/PHYS14/effAreaPhotons_cone03_pfNeutralHadrons_V2.txt"),
+                                 effAreaPhoFile   = cms.FileInPath
+                                 ("RecoEgamma/PhotonIdentification/data/PHYS14/effAreaPhotons_cone03_pfPhotons_V2.txt"),
+                                 genInfo = cms.InputTag("generator")
+                                )			   
+
+process.analysis = cms.Path(process.puppi + process.egmPhotonIsolationMiniAODPUPPI +  process.ntupler)
+
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.TFileService = cms.Service("TFileService",
+                                 fileName = cms.string("tree_miniAOD.root")
+                                  )
