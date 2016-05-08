2fd38d7a6b18e29065fd80e0f682ba3327942ff1
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-05-08 16:55:53 +0200

    add hOverE and sigmaIetaIeta cuts

--- PhotonNtupler/test/draw_ROC.cpp
+++ PhotonNtupler/test/draw_ROC.cpp
@@ -4,9 +4,9 @@
 void draw_ROC()
 {
   ROC_Drawer MyROCDrawer;
-  MyROCDrawer.FileName = "/afs/cern.ch/work/i/ishvetso/EgammaWork/PhotonIsolation_PUPPI_746_8July/CMSSW_7_4_6_patch2/src/EgammaWork/PhotonNtupler/test/crab_projects/crab_PhotonIsolation_PUPPI_miniAOD_8July2015/results/GJets.root";
-  MyROCDrawer.SigSelection = "pt > 20 && isTrue == 1";
-  MyROCDrawer.BkgSelection = "pt > 20 && ( isTrue == 0  )";
+  MyROCDrawer.FileName = "/afs/cern.ch/work/i/ishvetso/EgammaWork/photon_isolations/CMSSW_7_6_4/src/EgammaWork/PhotonNtupler/test/crab_projects/crab_PhotonIsolations/results/GJets.root";
+  MyROCDrawer.SigSelection = "( pt > 20 && isTrue == 1 ) && ( (isEB == 1 && hOverE < 0.05 && full5x5_sigmaIetaIeta < 0.01) || (isEB == 0 && hOverE < 0.05 && full5x5_sigmaIetaIeta < 0.0268)  )";
+  MyROCDrawer.BkgSelection = "( pt > 20 && ( isTrue != 1  ) ) && ( (isEB == 1 && hOverE < 0.05 && full5x5_sigmaIetaIeta < 0.01) || (isEB == 0 && hOverE < 0.05 && full5x5_sigmaIetaIeta < 0.0268)  )";
   MyROCDrawer.name = "ROC";
   MyROCDrawer.addSelection = "1";
   MyROCDrawer.draw_ROC();
