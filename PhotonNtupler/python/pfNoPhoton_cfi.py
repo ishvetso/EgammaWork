bebc6c18e64f2919fb884cc69add612736cb4bbf
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-05-12 01:50:12 +0200

    Delete pfNoPhoton_cfi.py

--- PhotonNtupler/python/pfNoPhoton_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-pfNoPhoton = cms.EDFilter("PFCandidateFwdPtrCollectionPdgIdFilter",
-    src = cms.InputTag("particleFlow"),
-    pdgId = cms.vint32(211,321,999211,2212,111,130,310,2112,11,13),
-    makeClones = cms.bool(True)
-)
