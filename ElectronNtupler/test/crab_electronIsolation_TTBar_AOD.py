c5765d311474e1a334a699ed2a0134bcfb23783c
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2016-12-01 23:56:20 +0100

    clean up crab configs

--- ElectronNtupler/test/crab_electronIsolation_TTBar_AOD.py
+++ ElectronNtupler/test/crab_electronIsolation_TTBar_AOD.py
@@ -1,19 +1,16 @@
 from WMCore.Configuration import Configuration
 config = Configuration()
 config.section_("General")
-config.General.requestName = 'Electron-Isolation_CITK_validation_ttbar_AOD_23June2015'
+config.General.requestName = 'Electron-AllIsolations-CITK-ttbar-1December2016'
 config.General.workArea = 'crab_projects'
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '/afs/cern.ch/work/i/ishvetso/EgammaWork/ElectronIsolationPUPPI_741_update19June2015/CMSSW_7_4_2_patch1/src/EgammaWork/electron_isolation_CITK_AOD.py'
 config.section_("Data")
-config.Data.inputDataset = '/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/AODSIM'
+config.Data.inputDataset = '/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16DR80Premix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/AODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 2 
-#config.Data.outLFN = '/store/user/<subdir>' # or '/store/group/<subdir>'
-config.Data.publication = True
-#config.Data.Data.publishDBS = 'phys03'
-config.Data.publishDataName = 'Electron-Isolation_CITK_validation_ttbar_AOD_23June2015'
+config.Data.publication = False
 config.section_("Site")
 config.Site.storageSite = 'T2_DE_DESY'
