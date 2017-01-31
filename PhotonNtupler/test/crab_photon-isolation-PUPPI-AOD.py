185181ac4321010adac3662c4221f67e187f78cd
Ivan Shvetsov <ivan.shvetsov@cern.ch>
2017-01-31 19:18:24 +0100

    cleanup

--- PhotonNtupler/test/crab_photon-isolation-PUPPI-AOD.py
+++ PhotonNtupler/test/crab_photon-isolation-PUPPI-AOD.py
@@ -1,19 +1,16 @@
 from WMCore.Configuration import Configuration
 config = Configuration()
 config.section_("General")
-config.General.requestName = 'PhotonIsolation_PUPPI_miniAOD_8July2015'
+config.General.requestName = 'PhotonIsolations_31January2017'
 config.General.workArea = 'crab_projects'
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = '/afs/cern.ch/work/i/ishvetso/EgammaWork/PhotonIsolation_PUPPI_746_8July/CMSSW_7_4_6_patch2/src/EgammaWork/photonIsolationPUPPI_miniAOD.py'
+config.JobType.psetName = '/afs/cern.ch/work/i/ishvetso/EgammaWork/PUPPI-isolation-moriond-samples-photons/CMSSW_8_0_21/src/EgammaWork/photonIsolationPUPPI_AOD.py'
 config.section_("Data")
-config.Data.inputDataset = '/GJet_Pt-15ToInf_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+config.Data.inputDataset = '/GJet_Pt-20toInf_DoubleEMEnriched_MGG-40to80_TuneCUETP8M1_13TeV_Pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1 
-#config.Data.outLFN = '/store/user/<subdir>' # or '/store/group/<subdir>'
-config.Data.publication = True
-#config.Data.Data.publishDBS = 'phys03'
-config.Data.publishDataName = 'PhotonIsolation_PUPPI_miniAOD_8July2015'
+config.Data.publication = False
 config.section_("Site")
 config.Site.storageSite = 'T2_DE_DESY'
